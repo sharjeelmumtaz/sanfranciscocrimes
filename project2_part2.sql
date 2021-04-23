4dcb90e8952bcbe1e3ae9259b8788e0fa698a607
Jonny Bischoff <jonnyb11299@gmail.com>
2021-04-23 16:46:55 -0500

    New procedureeeee (getcrimesbyxy)

--- project2_part2.sql
+++ project2_part2.sql
@@ -3,8 +3,6 @@ DROP DATABASE IF EXISTS SFIncidents;
 CREATE DATABASE SFIncidents;
 USE SFIncidents;
 
-select* from location;
-
 DROP TABLE IF EXISTS megatable;
 CREATE TABLE megatable (
 PdId BIGINT,
@@ -44,8 +42,10 @@ OWED_Public_Spaces_2_2 VARCHAR(255),
 Neighborhoods_2 VARCHAR(255)
 );
 
-LOAD DATA INFILE 'c:\\wamp64\\tmp\\police-department-incidents.csv'
-INTO TABLE megatable
+# LOAD DATA INFILE 'c:\\wamp64\\tmp\\police-department-incidents.csv'
+LOAD DATA
+    LOCAL INFILE '/Users/jonny/Documents/School/DBMS/project 2/police-department-incidents.csv'
+    INTO TABLE megatable 
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -113,6 +113,12 @@ HSOC_Zones_as_of_2018_06_05_2_2 VARCHAR(255),
 OWED_Public_Spaces_2_2 VARCHAR(255)
 );
 
+# We must change sql modes because the table we're working on has broken data,
+# and so we must group it to work around this.
+# Specifically, each entry is duplicated: so for every entry in the megatable,
+# there is another identical entry, including PdId, which is the primary key.
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
 INSERT INTO Description
 SELECT PdId,
 IncidntNum,
@@ -154,7 +160,7 @@ DELETE_Supervisor_Districts_2_2,
 DELETE_Zip_Codes_2_2,
 DELETE_Neighborhoods_2_2
 FROM megatable
-group by PdId;
+GROUP BY PdId;
 
 INSERT INTO Broken
 SELECT PdId,
@@ -170,7 +176,9 @@ Central_Market_Tenderloin_Boundary_Polygon_Updated_2_2,
 HSOC_Zones_as_of_2018_06_05_2_2,
 OWED_Public_Spaces_2_2
 FROM megatable
-group by PdId;
+GROUP BY PdId;
+
+SELECT * FROM Crime_Time;
 
 # Stored Procedure that gets the crimes on a cetain day of the week
 
@@ -194,9 +202,12 @@ DROP PROCEDURE IF EXISTS getCrimesByDate;
 DELIMITER //
 CREATE PROCEDURE getCrimesByDate(IN EnteredDate DATE)
 BEGIN 
-SELECT * 
+SELECT Crime_Time.PdId AS PdId, DayOfWeek, Crime_Time.Descript AS Descript, Incident_Date, Incident_Time,
+	IncidntNum, Incident_Code, Category, Resolution
 FROM Crime_Time
-WHERE Incident_Date = EnteredDate;
+	INNER JOIN Description USING (PdId)
+WHERE Incident_Date = EnteredDate
+ORDER BY Description.Descript ASC, Incident_Time ASC;
 END //
 DELIMITER ;
 
@@ -205,6 +216,7 @@ CALL getCrimesByDate('2004-07-02');
 
 # Stored Procedure that gets the crimes in a certain district
 
+DROP PROCEDURE IF EXISTS getCrimesbyDistrict;
 DELIMITER //
 CREATE PROCEDURE getCrimesbyDistrict(IN EnteredDistrict VARCHAR(255))
 BEGIN
@@ -219,6 +231,7 @@ CALL getCrimesByDistrict('Bayview');
 
 # Stored Procedure that gets the crimes by a certain category
 
+DROP PROCEDURE IF EXISTS getCrimesbyCategory;
 DELIMITER //
 CREATE PROCEDURE getCrimesbyCategory(IN EnteredCategory VARCHAR(255))
 BEGIN
@@ -230,6 +243,24 @@ DELIMITER ;
 
 # Testing the location
 CALL getCrimesByCategory('DRUG/NARCOTIC');
+
+# Stored Procedure that gets the crimes based on latitude and longitude
+DROP PROCEDURE IF EXISTS getCrimesByXY;
+DELIMITER //
+CREATE PROCEDURE getCrimesByXY(IN minx FLOAT, IN maxx FLOAT, IN miny FLOAT, IN maxy FLOAT)
+BEGIN
+SELECT Description.PdId AS PdId, Category, Description.Descript AS Descript, Resolution,
+	PdDistrict, Address, X, Y, Incident_Date, Incident_Time
+FROM Description
+	INNER JOIN Location USING (PdId)
+    INNER JOIN Crime_Time USING (PdId)
+WHERE X >= minx AND X <= maxx AND Y >= miny AND Y <= maxy
+LIMIT 100;
+END //
+DELIMITER ;
+
+# Testing the above procedure
+CALL getCrimesByXY(-122.514, -122.499, 37.7079, 37.78);
 
 
 # Trigger
